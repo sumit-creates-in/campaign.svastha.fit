78c742ecb4ddaea0d8077d8d5dfb61f207286a27
Ankit Gupta <ankitgupta8208@gmail.com>
2026-07-03 10:22:26 +0530

    loading

--- src/pages/Ultimate21DayChallenge.tsx
+++ src/pages/Ultimate21DayChallenge.tsx
@@ -110,6 +110,7 @@ const Ultimate21DayChallenge = () => {
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-green-600 mx-auto mb-4"></div>
           <p className="text-gray-600">Loading...</p>
         </div>
+        <WhatsAppFloatingButton showImmediately={true} />
       </div>
     );
   }
