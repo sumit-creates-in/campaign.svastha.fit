30147fc580274f821529dc212e6f1b8d86f569a7
Sachin Gupta <sgupta80903@gmail.com>
2026-07-16 10:08:41 +0530

    update link

--- src/pages/RegistrationConfirm21WLYC.tsx
+++ src/pages/RegistrationConfirm21WLYC.tsx
@@ -136,8 +136,8 @@ const RegistrationConfirm21WLYC = ({
     const whatsappLink = isUsa
       ? "https://chat.whatsapp.com/Jvz15JL1sdBGQNDIcj4c8z?s=cl&p=a&ilr=4"
       : isIndia
-        ? "https://chat.whatsapp.com/Lhyb7jWRiMw3zAqgjJVMMA"
-        : "https://chat.whatsapp.com/Lhyb7jWRiMw3zAqgjJVMMA";
+        ? "https://chat.whatsapp.com/LMuS8Wx5w0VAt0gU1sGCTI?s=cl&p=a&ilr=4"
+        : "https://chat.whatsapp.com/LMuS8Wx5w0VAt0gU1sGCTI?s=cl&p=a&ilr=4";
     window.open(whatsappLink, "_blank");
   };
 
